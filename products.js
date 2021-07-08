b0f55360f818123906e045a8b2a72a4c313e7c98
kristenlynodd <kristenlyndodd@gmail.com>
2021-07-08 11:49:01 -0700

    added html & arrays

---
+++ products.js
@@ -0,0 +1,10 @@
+import vegetables from './vegetables.js';
+import { renderVegetables } from './vegetable-render.js';
+
+const vegetableUL = document.getElementById('vegetables');
+
+for (let vegetable of vegetables) {
+    const vegetableLI = renderVegetables(vegetable);
+    vegetableUL.appendChild(vegetableLI);
+}
+
---
+++ vegtables.js
@@ -0,0 +1,52 @@
+const carrots = {
+    id: 'carrots',
+    name: 'Carrots',
+    image: './assets/carrots.jpg',
+    description: 'orange, and crunchy',
+    category: 'root',
+    price: 0.50
+};
+
+const radishes = {
+    id: 'radishes',
+    name: 'Radishes',
+    image: './assets/radishes.jpg',
+    description: 'small, spicy and crunchy',
+    category: 'root',
+    price: 0.25
+};
+const celery = {
+    id: 'celery',
+    name: 'Celery',
+    image: './assets/celery.jpg',
+    description: 'leafy green, nutrient rich',
+    category: 'greens',
+    price: 3.00
+};
+const onions = {
+    id: 'onions',
+    name: 'Onions',
+    image: './assets/onions.jpg',
+    description: 'aromatic that adds flavor to dishes when cooked',
+    category: 'aromatic',
+    price: 1.50
+};
+
+const broccoli = {
+    id: 'broccoli',
+    name: 'Broccoli',
+    image: './assets/broccoli.jpg',
+    description: 'best sauteed, roasted, or steamed. Nutritious and flavorful',
+    category: 'floret',
+    price: 2.50
+};
+
+const vegetables = [
+    carrots,
+    radishes,
+    celery,
+    onions,
+    broccoli,
+   
+];
+export default vegetables;
